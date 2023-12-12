d05aef7c19121936e970ee9fefb48927b12f5903
pscon <ekunolapaul@gmail.com>
2023-12-12 18:33:45 +0100

    fixing errors

--- src/components/AddContacts.js
+++ src/components/AddContacts.js
@@ -1,63 +1,60 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class AddContacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      phone: "",
-    };
-  }
+const AddContacts = ({ addContact }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+  });
 
-  handleChange = (inputType, e) => {
-    if (inputType === "name") {
-      this.setState({
-        name: e.target.value,
-      });
-      return;
-    }
-    this.setState({
-      phone: e.target.value,
+  const { name, phone } = formData;
+
+  const handleChange = (inputType, e) => {
+    setFormData({
+      ...formData,
+      [inputType]: e.target.value,
     });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, phone } = this.state;
-    const { addContact } = this.props;
-    if (name && phone) {
-      addContact(name, phone);
-      this.setState({
-        name: "",
-        phone: "",
-      });
+
+    if (name.trim() === "" || phone.trim() === "") {
+      // Add your form validation logic here
+      alert("Please fill in all fields");
+      return;
     }
+
+    addContact(name, phone);
+
+    setFormData({
+      name: "",
+      phone: "",
+    });
   };
 
-  render() {
-    const { name, phone } = this.state;
-    return (
-      <div id="add-contacts-container">
-        <h1>Add Contact</h1>
-        <form>
-          <input
-            placeholder="Enter Name"
-            value={name}
-            required
-            onChange={(e) => this.handleChange("name", e)}
-          />
-          <input
-            placeholder="Enter Phone"
-            value={phone}
-            required
-            onChange={(e) => this.handleChange("phone", e)}
-          />
-          <br />
-          <button onClick={this.handleSubmit}>Add Contact</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div id="add-contacts-container">
+      <h1>Add Contact</h1>
+      <form>
+        <input
+          placeholder="Enter Name"
+          value={name}
+          required
+          type="text"
+          onChange={(e) => handleChange("name", e)}
+        />
+        <input
+          placeholder="Enter Phone"
+          value={phone}
+          type="tel"
+          required
+          onChange={(e) => handleChange("phone", e)}
+        />
+        <br />
+        <button onClick={handleSubmit}>Add Contact</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddContacts;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,99 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editMode: false,
-      editedName: "",
-      editedPhone: "",
-    };
-  }
+const ListItem = ({ name, contact, handleUpdate, handleDelete, id }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [editedName, setEditedName] = useState("");
+  const [editedPhone, setEditedPhone] = useState("");
 
-  handleEdit = () => {
-    this.setState({
-      editMode: true,
-    });
+  const handleEdit = () => {
+    setEditMode(true);
   };
 
-  handleNameChange = (e) => {
-    // console.log(e.target.value);
-    this.setState({
-      editedName: e.target.value,
-    });
+  const handleNameChange = (e) => {
+    setEditedName(e.target.value);
   };
 
-  handlePhoneChange = (e) => {
-    // console.log(e.target.value);
-    this.setState({
-      editedPhone: e.target.value,
-    });
+  const handlePhoneChange = (e) => {
+    setEditedPhone(e.target.value);
   };
 
-  handleUpdateContact = async () => {
-    const { editedName, editedPhone } = this.state;
-    const { handleUpdate, id } = this.props;
+  const handleUpdateContact = async () => {
     if (editedName && editedPhone) {
       await handleUpdate(editedName, editedPhone, id);
-      this.setState({
-        editMode: false,
-      });
+      setEditMode(false);
     }
   };
 
-  render() {
-    const { name, contact, handleDelete, id } = this.props;
-    const { editMode } = this.state;
-    return (
-      <li>
-        <p className="name-container">
-          {editMode ? (
-            <input
-              placeholder="Name..."
-              onChange={this.handleNameChange}
-              required
-            />
-          ) : (
-            name
-          )}
-        </p>
-        <p className="phone-container">
-          {editMode ? (
-            <input
-              placeholder="Phone..."
-              onChange={this.handlePhoneChange}
-              required
-            />
-          ) : (
-            contact
-          )}
-        </p>
-        <p className="btns-container">
-          {editMode ? (
-            <img
-              className="list-btn"
-              onClick={this.handleUpdateContact}
-              src="https://cdn-icons-png.flaticon.com/512/1688/1688988.png"
-              alt="submit-edit"
-            />
-          ) : (
-            <img
-              className="list-btn"
-              onClick={this.handleEdit}
-              src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png"
-              alt="edit-btn"
-            />
-          )}
+  return (
+    <li>
+      <p className="name-container">
+        {editMode ? (
+          <input placeholder="Name..." onChange={handleNameChange} required />
+        ) : (
+          name
+        )}
+      </p>
+      <p className="phone-container">
+        {editMode ? (
+          <input placeholder="Phone..." onChange={handlePhoneChange} required />
+        ) : (
+          contact
+        )}
+      </p>
+      <p className="btns-container">
+        {editMode ? (
           <img
             className="list-btn"
-            onClick={() => handleDelete(id)}
-            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
-            alt="delete-btn"
+            onClick={handleUpdateContact}
+            src="https://cdn-icons-png.flaticon.com/512/1688/1688988.png"
+            alt="submit-edit"
           />
-        </p>
-      </li>
-    );
-  }
-}
+        ) : (
+          <img
+            className="list-btn"
+            onClick={handleEdit}
+            src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png"
+            alt="edit-btn"
+          />
+        )}
+        <img
+          className="list-btn"
+          onClick={() => handleDelete(id)}
+          src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+          alt="delete-btn"
+        />
+      </p>
+    </li>
+  );
+};
 
 export default ListItem;
